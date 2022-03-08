f516ca387d6219ba49f85144da6579c902cb407b
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-03-08 13:21:21 -0800

    commit tree 41099e018ba5f7f09fd90d85175ea0e5db0f6a70

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-
+    # setter method
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+      end
+  
+    # getter method
+    def name
+        @this_dogs_name
+    end
 end
+  
+odie = Dog.new
+odie.name = "Odie"
+  
+# call the Dog#name method
+puts odie.name
+# "Odie"
